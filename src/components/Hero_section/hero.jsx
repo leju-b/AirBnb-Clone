e5c13786f6c4e072b0b03100f19c4e106efdd725
leju b <iamleju02@gmail.com>
2023-06-06 11:17:31 +0530

    changed

---
+++ src/components/Hero_section/hero.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './hero.css';
+
+const Hero = () =>{
+    return(
+        <section>
+            <div className="collage" >
+                <img src="./images/Group 77.png" alt="" />
+            </div>
+        </section>
+    )
+}
+
+export default Hero;
