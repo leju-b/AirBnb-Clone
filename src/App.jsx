a15191c6d35d8fc9196577ba1b49013cd127e117
leju b <iamleju02@gmail.com>
2023-06-30 13:38:51 +0530

    props

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import NavBar from "./components/Header/header"
 import Hero from "./components/Hero_section/hero"
 import Section_one from "./components/Section1/section1"
-
+import data from "./components/Section1/data"
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const Dataelemets = data.map(datas=>{
+    return <Section_one
+          {...datas}
+        //  datas = {datas}
+        // coverImg = {datas.coverImg}
+        // rating={datas.stats.rating}
+        // id  ={datas.id}
+        // location ={datas.location}
+        // title ={datas.title}
+        // price ={datas.price}
+        // openSpots = {datas.openSpots}
+    />
+  })
 
   return(
     <div>
       <NavBar/>
       <Hero />
-      <Section_one />
+      <section className="card-section">
+        {Dataelemets}
+      </section>
+      
     </div>
   )
 }
--- src/components/Section1/section1.jsx
+++ src/components/Section1/section1.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import './section1.css'
 
-const Section_one = () => {
+const Section_one = (props) => {
+    let badgeText
+    if (props.openSpots === 0){
+        badgeText = "SOLD OUT"
+    }else if(props.location === "Online"){
+        badgeText = "Online"
+    } 
     return(
-        <section className="card-section">
             <div className="card-one">
-                <img src="./images/image 12.png" alt="" />
+                {badgeText && <div className="bText">{badgeText}</div>}
+                <img src="./images/image 12.png" alt="" className="CoverImg"/>
                 <div className="card--details">
-                    <p className="star"><img src="./images/Star 1.png" alt="" /><span><strong>5.0 </strong></span>(6) - USA</p>
-                    <p className="summary">Life lessons with Katie Zaferes</p>
-                    <p className="price"><span>From </span><span>$136</span> / person</p>
+                    <p className="star"><img src="./images/Star 1.png" alt="" className="starImg" /> <span><strong>{props.stats.rating} </strong></span>({props.id}) - {props.location}</p>
+                    <p className="summary">{props.title}</p>
+                    <p className="price"><strong><span>From </span><span className="price">${props.price}</span></strong> / person</p>
                 </div>
             </div>
-        </section>
     )
 }
 
